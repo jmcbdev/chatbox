8d9789c6177717c32eaac0fdfc8bf39ec8f46005
kudakadira <kadirakuda@gmail.com>
2020-10-14 01:00:00 +0100

    Making a few updates

--- scriptChat.py
+++ scriptChat.py
@@ -5,6 +5,9 @@ import pyttsx3
 import wikipedia
 from pygame import mixer
 import speech_recognition as sr
+from threading import Thread
+import requests
+from bs4 import BeautifulSoup
 
 
 engine = pyttsx3.init()
@@ -19,22 +22,6 @@ greetings = ['hey there', 'hello', 'hi', 'Hai', 'hey!', 'hey']
 question = ['how are you', 'how are you doing']
 responses = ['Okay', "I'm fine"]
 
-r = sr.Recognizer()
-
-with sr.Microphone() as source:
-    print("Say something..")
-    audio = r.listen(source)
-
-    # recognize speech using Google Speech Recognition
-    try:
-        # for testing purposes, we're just using the default API key
-        # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
-        # instead of `r.recognize_google(audio)`
-
-        print(f"You said: {r.recognize_google(audio)}")
-
-    except LookupError:
-        print("Sorry could not recognize your voice.")
 
 
 if r.recognize_google(audio) in greetings:
@@ -58,3 +45,33 @@ else:
    engine.runAndWait()
    userInput3 = input("or else search in google")
    webbrowser.open_new('www.google.com/search?q=' + userInput3)
+
+
+def user_input():
+    global query
+    r = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        print("Say something..")
+        audio = r.listen(source)
+
+        # recognize speech using Google Speech Recognition
+        try:
+            # for testing purposes, we're just using the default API key
+            # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+            # instead of `r.recognize_google(audio)`
+            query = r.recognize_google(audio, language='en-in')
+            print(f"You said: {query}")
+
+        except LookupError:
+            print("Sorry could not recognize your voice.")
+
+def main_windon():
+      global query
+      while True:
+          if query != None:
+              if 'shutdown' in query or 'quit' in query or 'stop' in query or 'goodbye' in query:
+                  shut_down()
+              else:
+                  web_scraping(query)
+                  query = None
